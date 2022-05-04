225e7e5bcb564a4c0f64e0b54ae919f93958b387
realllsiyu <siyuc0712@gmail.com>
2022-05-04 13:03:37 -0400

    Set up

--- webpack.config.js
+++ webpack.config.js
@@ -9,3 +9,18 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
 const dirApp = path.join(__dirname, "app");
 const dirAssets = path.join(__dirname, "assets");
 const dirStyles = path.join(__dirname, "styles");
+const dirNode = "node_modules";
+
+module.exports = {
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+
+  resolve: {
+    modules: [dirApp, dirAssets, dirStyles, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+  ],
+};
